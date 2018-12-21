749a3a673a5d06ec250c72f07d7cee41c58a1d87
melinamejia95 <melinamejia95@gmail.com>
2018-12-21 11:51:28 -0500

    Customize card

--- bin/card.js
+++ bin/card.js
@@ -19,6 +19,7 @@ const data = {
   work: chalk.white('Front-end developer at Seven4N'),
   twitter: chalk.cyan('https://twitter.com/melinamejia95'),
   github: chalk.cyan('https://github.com/MelinaMejia95'),
+  linkedin: chalk.cyan('https://linkedin.com/in/melinamejia95'),
   npx: chalk.white('npx melinamejia'),
   labelWork: chalk.white.bold('      Work:'),
   labelTwitter: chalk.white.bold('   Twitter:'),
@@ -29,15 +30,14 @@ const data = {
 
 // Actual strings we're going to output
 const newline = '\n'
-const heading = `${data.name} ${data.handle}`
+const heading = `${data.name} 🎨 ${data.handle}`
 const working = `${data.labelWork}  ${data.work}`
 const twittering = `${data.labelTwitter}  ${data.twitter}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
 const carding = `${data.labelCard}  ${data.npx}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline + webing + newline + newline + carding
+const output = heading + newline + newline + working + newline + twittering + newline + githubing + newline + linkedining + newline  + carding
 
 console.log(chalk.green(boxen(output, options)))
